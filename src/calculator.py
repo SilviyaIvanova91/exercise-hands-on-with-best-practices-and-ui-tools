4332f2f72b35426e969561447877075fa6a48d80
Student <silviya.yordanova91@gmail.com>
2026-06-19 15:51:52 +0300

    Adding multiply functionality

--- src/calculator.py
+++ src/calculator.py
@@ -2,14 +2,12 @@ def add(first_number: int, second_number: int):
     """Return the sum of two numbers."""
     return first_number + second_number
 
-def substract(first_number: int, second_number:int):
+
+def substract(first_number: int, second_number: int):
     """Return the difference of two numbers."""
     return first_number - second_number
+
 
 def multiply(first_number: int, second_number: int):
     """Return the product of two numbers."""
     return first_number * second_number
-
-
-
-
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -6,10 +6,12 @@ def test_add():
     assert add(-1, -1) == -2
     assert add(0, 0) == 0
 
+
 def test_substract():
     assert substract(5, 3) == 2
     assert substract(-1, -1) == 0
     assert substract(0, 5) == -5
+
 
 def test_multiply():
 
