9e3612b1ab166323d0bf8c907a57a2a8847dc354
Student <silviya.yordanova91@gmail.com>
2026-06-19 15:23:42 +0300

    Adding and install formating of the fails

--- src/calculator.py
+++ src/calculator.py
@@ -1,10 +1,10 @@
 def add(first_number: int, second_number: int):
     """Return the sum of two numbers."""
-    return first_number + second_number;
+    return first_number + second_number
 
 def substract(first_number: int, second_number:int):
     """Return the difference of two numbers."""
-    return first_number - second_number;
+    return first_number - second_number
 
 print(add(5,3))
 
