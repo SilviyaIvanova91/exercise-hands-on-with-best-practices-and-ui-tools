62f99947942e3f868a68c76817851284baab5aa2
Student <silviya.yordanova91@gmail.com>
2026-06-19 15:11:46 +0300

    Adding unti test for basic calculator

---
+++ src/calculator.py
@@ -0,0 +1,5 @@
+def add(first_number: int, second_number: int):
+    return first_number + second_number
+
+def substract(first_number: int, second_number: int):
+    return first_number - second_number
